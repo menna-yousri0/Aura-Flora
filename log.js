4977c2ca85e94872c744f1a67f1bf5db6d7c2de4
menna-yousri0 <mennayousri044@gmail.com>
2026-04-23 21:36:38 +0200

    first commit

---
+++ log.js
@@ -0,0 +1,44 @@
+let welcome= document.getElementById('welcome');
+let register= document.getElementById('register');
+let login= document.getElementById('login');
+let btncreat= document.getElementsByClassName('creat')[0];
+let btnlog= document.getElementsByClassName('log')[0];
+let row= document.getElementsByClassName('row')[0];
+let arrow=  document.getElementById('arrow');
+let arrowlog= document.getElementsByClassName('arrow')[0];
+let dark=document.getElementsByClassName('mode-images')[0];
+let or= document.getElementById('or');
+btncreat.onclick= function(){
+    welcome.classList.add('hide');
+    register.classList.remove('hide');
+    row.style.backgroundColor='rgba(233, 160, 224, 0.99)'; ;
+}
+btnlog.onclick= function(){
+    welcome.classList.add('hide');
+    login.classList.remove('hide');
+    row.style.backgroundColor='rgba(253, 244, 252, 0.99)';
+}
+arrow.onclick= function(){ 
+    login.classList.add('hide');
+    register.classList.add('hide');
+    welcome.classList.remove('hide');
+    row.style.backgroundColor='rgba(233, 160, 224, 0.99)'; ; 
+}
+arrowlog.onclick = function(){
+    login.classList.add("hide");
+    welcome.classList.remove('hide');
+    row.style.backgroundColor='rgba(233, 160, 224, 0.99)'; 
+}
+dark.onclick=function toggleDarkMode(){
+    
+   
+               document.body.classList.toggle('dark-mode');
+            
+           
+            if (document.body.classList.contains('dark-mode')) {
+                localStorage.setItem('dark-mode', 'enabled');
+            } else {
+                localStorage.setItem('dark-mode', 'disabled');
+            }
+        }
+
